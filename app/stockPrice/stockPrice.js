f9cb659b80b5a0826c7593c5ad70f1bb4a38b3aa
Chris Davis <chris@davislabs.co>
2016-07-20 22:20:22 -0500

    1st attempt at pulling historical stock price data into stock price widget from yahoo financials.  No errors in console, but not quite working either.
    Chris Davis
    2016-07-20
    22:20:21

--- app/stockPrice/stockPrice.js
+++ app/stockPrice/stockPrice.js
@@ -10,18 +10,20 @@ angular.module('tgdashboard.stockPrice', ['ngResource'])
 
   var url = baseUrl + '?q=' + data + '&format=json&diagnostics=true&env=http://datatables.org/alltables.env';
 
-  var resource = $resource(url, {
-      callback: "JSON_CALLBACK"
-  }, {
-      getQuote: {
-          method: "JSONP",
-          isArray: false
-      }
-  });
+  var resource = $resource(url, {callback: "JSON_CALLBACK"}, {getQuote: {method: "JSONP", isArray: false}});
+      //http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.historicaldata where symbol = "YHOO" and startDate = "2014-02-11" and endDate = "2014-02-18"&diagnostics=true&env=store://datatables.org/alltableswithkeys
+
+  var historydata = encodeURIComponent("select * from yahoo.finance.historicaldata where symbol in ('" + symbol + "') and startDate = '2016-01-01' and endDate = '2016-07-21'");
+
+  var historyurl = baseUrl + '?q=' + historydata + '&format=json&diagnostics=true&env=http://datatables.org/alltables.env';
+
+  var historyresource = $resource(historyurl, {callback: "JSON_CALLBACK"}, {getQuote: {method: "JSONP", isArray: false}});
+  //});
 
   resource.getQuote().$promise.then(
       function(data) {
           $scope.lastTradePriceOnly = data.query.results.quote.LastTradePriceOnly;
+          $scope.historydata = data.query.results.quote.Close;
       },
       function(error) {
           // If something goes wrong with a JSONP request in AngularJS,
