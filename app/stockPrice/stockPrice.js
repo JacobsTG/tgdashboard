baa56c0eb4e2bfb29a773f8c96d1078872b2f391
Michael Lynch <mplynch@gmail.com>
2016-07-21 21:02:17 -0500

    Fixed stock history JS code & added view example
    Changed the stock price module to ensure two HTTP requests were made to
    get the current price and history for a specified date interval.  Added
    example HTML markup to demonstrate how to access the array of history
    elements.

--- app/stockPrice/stockPrice.js
+++ app/stockPrice/stockPrice.js
@@ -6,30 +6,61 @@ angular.module('tgdashboard.stockPrice', ['ngResource'])
 
   var baseUrl = 'http://query.yahooapis.com/v1/public/yql';
   var symbol = 'JEC';
-  var data = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('" + symbol + "')");
 
-  var url = baseUrl + '?q=' + data + '&format=json&diagnostics=true&env=http://datatables.org/alltables.env';
+  // Set up the request address and query string
+  var queryString = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('" + symbol + "')");
+  var url = baseUrl + '?q=' + queryString + '&format=json&diagnostics=true&env=http://datatables.org/alltables.env';
 
-  var resource = $resource(url, {callback: "JSON_CALLBACK"}, {getQuote: {method: "JSONP", isArray: false}});
-      //http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.historicaldata where symbol = "YHOO" and startDate = "2014-02-11" and endDate = "2014-02-18"&diagnostics=true&env=store://datatables.org/alltableswithkeys
+  // Set up a request using the $resource service
+  var currentPriceResource = $resource(url, {
+    callback: "JSON_CALLBACK"
+  }, {
+    getQuote: {
+      method: "JSONP",
+      isArray: false
+    }
+  });
 
-  var historydata = encodeURIComponent("select * from yahoo.finance.historicaldata where symbol in ('" + symbol + "') and startDate = '2016-01-01' and endDate = '2016-07-21'");
-
-  var historyurl = baseUrl + '?q=' + historydata + '&format=json&diagnostics=true&env=http://datatables.org/alltables.env';
-
-  var historyresource = $resource(historyurl, {callback: "JSON_CALLBACK"}, {getQuote: {method: "JSONP", isArray: false}});
-  //});
-
-  resource.getQuote().$promise.then(
-      function(data) {
-          $scope.lastTradePriceOnly = data.query.results.quote.LastTradePriceOnly;
-          $scope.historydata = data.query.results.quote.Close;
-      },
-      function(error) {
-          // If something goes wrong with a JSONP request in AngularJS,
-          // the status code is always reported as a "0". As such, it's
-          // a bit of black-box, programmatically speaking.
-          alert("Something went wrong!");
-      }
+  // Make the request for the current price
+  currentPriceResource.getQuote().$promise.then(
+    function(data) {
+      $scope.lastTradePriceOnly = data.query.results.quote.LastTradePriceOnly;
+    },
+    function(error) {
+      // If something goes wrong with a JSONP request in AngularJS,
+      // the status code is always reported as a "0". As such, it's
+      // a bit of black-box, programmatically speaking.
+      alert("Something went wrong!");
+    }
   );
+
+  //http://query.yahooapis.com/v1/public/yql?q=select * from yahoo.finance.historicaldata where symbol = "YHOO" and startDate = "2014-02-11" and endDate = "2014-02-18"&diagnostics=true&env=store://datatables.org/alltableswithkeys
+
+  // Set up the request address and query string
+  queryString = encodeURIComponent("select * from yahoo.finance.historicaldata where symbol in ('" + symbol + "') and startDate = '2016-01-01' and endDate = '2016-07-21'");
+  var historyurl = baseUrl + '?q=' + queryString + '&format=json&diagnostics=true&env=http://datatables.org/alltables.env';
+
+  // Set up a request using the $resource service
+  var historyresource = $resource(historyurl, {
+    callback: "JSON_CALLBACK"
+  }, {
+    getHistory: {
+      method: "JSONP",
+      isArray: false
+    }
+  });
+
+  // Make the request for the price history
+  historyresource.getHistory().$promise.then(
+    function(data) {
+      $scope.priceHistory = data.query.results.quote;
+    },
+    function(error) {
+      // If something goes wrong with a JSONP request in AngularJS,
+      // the status code is always reported as a "0". As such, it's
+      // a bit of black-box, programmatically speaking.
+      alert("Something went wrong!");
+    }
+  );
+
 }]);
